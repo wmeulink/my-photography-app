8fe52197c7b5aa22151bc41b691b1e13aa76e8aa
Whitney Meulink <wmeulink@gmail.com>
2025-10-09 08:23:18 -0700

    develop in a clean state

--- src/components/Portraits.jsx
+++ src/components/Portraits.jsx
@@ -54,8 +54,7 @@ export default function Portraits() {
     setCurrentIndex(index);
     setLightboxOpen(true);
   };
-  console.log("categories", category);
-  console.log("portraits", portraits)
+
   return (
     <div className="portraits-container">
       <Box sx={{ p: 4 }} className="page-container">
