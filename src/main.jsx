ba91b0ab467359d748df125a14a680f300010969
Whitney Meulink <wmeulink@gmail.com>
2026-03-12 11:04:44 -0700

    wrapping main.jsx in <HelmetProvider>, issue happening in the js bundle

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // note the "/client"
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async"; // <-- import
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>   {/* <-- wrap the app */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
