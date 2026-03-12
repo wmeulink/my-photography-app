570a047a27d39fc014ac7c7ce125a0c4dd414d2a
Whitney Meulink <wmeulink@gmail.com>
2026-03-12 08:29:31 -0700

    bleh

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode> <BrowserRouter> <App /> </BrowserRouter>
+</React.StrictMode>
 );
