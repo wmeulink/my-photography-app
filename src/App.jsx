da8e4ffe0a0df92ac1c08a1106323ffec9ed7cd2
Whitney Meulink <wmeulink@gmail.com>
2026-03-16 11:04:25 -0700

    slight changes to header, footer, plus hamburger menu for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,32 @@ import Portraits from './components/Portraits';
 import "./App.css";
 
 function App() {
-
   return (
     <div className="navbar-container">
       <header className="site-header">
         <h1>Elliott Photography</h1>
         <Navbar />
       </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/albums" element={<LandscapeAlbums />} />
-        <Route path="/landscapes" element={<Landscapes />} />
-        <Route path="/landscapes/:category" element={<Landscapes />} />
-        <Route path="/portraits" element={<Portraits />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+
+      <main className="site-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/albums" element={<LandscapeAlbums />} />
+          <Route path="/landscapes" element={<Landscapes />} />
+          <Route path="/landscapes/:category" element={<Landscapes />} />
+          <Route path="/portraits" element={<Portraits />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+
       <div className='footer-container'>
-      <footer className='footer'>
-        <p>© 2025 Developed & Designed by Whitney Elliott</p>
-      </footer>
+        <footer className='footer'>
+          <p>© 2026 Developed & Designed by Whitney Elliott</p>
+        </footer>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
